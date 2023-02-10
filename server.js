d679314cd078f2bb775d6832399d2d85e8aa2d5a
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-02-10 10:07:38 +0530

    connect db

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8888;
 
+// DB Connection
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB CONNECTED successfully!');
+  });
+
 app.get('/', (req, res) => {
   res.send('backend is running!');
 });
