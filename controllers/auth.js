e23c156319f3e197b474ca2611ece908af19367c
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-02-10 10:32:23 +0530

    more controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -77,3 +77,18 @@ exports.isSignedIn = expressJwt({
   userProperty: 'auth',
   algorithms: ['HS256'],
 });
+
+// custom middlewares
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!checker) return res.status(403).json({ error: 'Access denied' });
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0)
+    return res
+      .status(403)
+      .json({ error: 'No Admin permissions found, access denied!' });
+  next();
+};
